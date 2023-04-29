c13276655dd97c65c8ce338bcdc2a479fe2aabf5
Melvillian <melvillevt@gmail.com>
2023-04-29 12:40:41 -0400

    refactored so that the whole pipeline of computation happens per-file, which eases debugging by not having to redo everything if something fails partway through

--- README.md
+++ README.md
@@ -1,56 +1,4 @@
 
-# Python Project Template
-
-A low dependency and really simple to start project template for Python Projects.
-
-See also 
-- [Flask-Project-Template](https://github.com/rochacbruno/flask-project-template/) for a full feature Flask project including database, API, admin interface, etc.
-- [FastAPI-Project-Template](https://github.com/rochacbruno/fastapi-project-template/) The base to start an openapi project featuring: SQLModel, Typer, FastAPI, JWT Token Auth, Interactive Shell, Management Commands.
-
-### HOW TO USE THIS TEMPLATE
-
-> **DO NOT FORK** this is meant to be used from **[Use this template](https://github.com/rochacbruno/python-project-template/generate)** feature.
-
-1. Click on **[Use this template](https://github.com/rochacbruno/python-project-template/generate)**
-3. Give a name to your project  
-   (e.g. `my_awesome_project` recommendation is to use all lowercase and underscores separation for repo names.)
-3. Wait until the first run of CI finishes  
-   (Github Actions will process the template and commit to your new repo)
-4. If you want [codecov](https://about.codecov.io/sign-up/) Reports and Automatic Release to [PyPI](https://pypi.org)  
-  On the new repository `settings->secrets` add your `PYPI_API_TOKEN` and `CODECOV_TOKEN` (get the tokens on respective websites)
-4. Read the file [CONTRIBUTING.md](CONTRIBUTING.md)
-5. Then clone your new project and happy coding!
-
-> **NOTE**: **WAIT** until first CI run on github actions before cloning your new project.
-
-### What is included on this template?
-
-- 🖼️ Templates for starting multiple application types:
-  * **Basic low dependency** Python program (default) [use this template](https://github.com/rochacbruno/python-project-template/generate)
-  * **Flask** with database, admin interface, restapi and authentication [use this template](https://github.com/rochacbruno/flask-project-template/generate).
-  **or Run `make init` after cloning to generate a new project based on a template.**
-- 📦 A basic [setup.py](setup.py) file to provide installation, packaging and distribution for your project.  
-  Template uses setuptools because it's the de-facto standard for Python packages, you can run `make switch-to-poetry` later if you want.
-- 🤖 A [Makefile](Makefile) with the most useful commands to install, test, lint, format and release your project.
-- 📃 Documentation structure using [mkdocs](http://www.mkdocs.org)
-- 💬 Auto generation of change log using **gitchangelog** to keep a HISTORY.md file automatically based on your commit history on every release.
-- 🐋 A simple [Containerfile](Containerfile) to build a container image for your project.  
-  `Containerfile` is a more open standard for building container images than Dockerfile, you can use buildah or docker with this file.
-- 🧪 Testing structure using [pytest](https://docs.pytest.org/en/latest/)
-- ✅ Code linting using [flake8](https://flake8.pycqa.org/en/latest/)
-- 📊 Code coverage reports using [codecov](https://about.codecov.io/sign-up/)
-- 🛳️ Automatic release to [PyPI](https://pypi.org) using [twine](https://twine.readthedocs.io/en/latest/) and github actions.
-- 🎯 Entry points to execute your program using `python -m <rekt_summarizer>` or `$ rekt_summarizer` with basic CLI argument parsing.
-- 🔄 Continuous integration using [Github Actions](.github/workflows/) with jobs to lint, test and release your project on Linux, Mac and Windows environments.
-
-> Curious about architectural decisions on this template? read [ABOUT_THIS_TEMPLATE.md](ABOUT_THIS_TEMPLATE.md)  
-> If you want to contribute to this template please open an [issue](https://github.com/rochacbruno/python-project-template/issues) or fork and send a PULL REQUEST.
-
-[❤️ Sponsor this project](https://github.com/sponsors/rochacbruno/)
-
-<!--  DELETE THE LINES ABOVE THIS AND WRITE YOUR PROJECT README BELOW -->
-
----
 # rekt_summarizer
 
 [![codecov](https://codecov.io/gh/Melvillian/rekt_summarizer/branch/main/graph/badge.svg?token=rekt_summarizer_token_here)](https://codecov.io/gh/Melvillian/rekt_summarizer)
@@ -58,21 +6,13 @@ See also
 
 Awesome rekt_summarizer created by Melvillian
 
-## Install it from PyPI
+## Installation
 
 ```bash
-pip install rekt_summarizer
+pip install -r requirements.txt
 ```
 
 ## Usage
-
-```py
-from rekt_summarizer import BaseClass
-from rekt_summarizer import base_function
-
-BaseClass().base_method()
-base_function()
-```
 
 ```bash
 $ python -m rekt_summarizer
@@ -80,6 +20,33 @@ $ python -m rekt_summarizer
 $ rekt_summarizer
 ```
 
-## Development
+This will:
+1. Fetch the rekt.news articles listed in their [leaderboard](https://rekt.news/leaderboard/)
+2. Convert the HTML articles to markdown and store them in [rekt_summarizer/markdown_data/](./rekt_summarizer/markdown_data/)
+3. Use OpenAI to categorize the hacks involved in each article such the tool outputs a JSON object of the form:
 
-Read the [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+```js
+{
+  categories: {
+    contract_manipulation: 75,
+    reentrancy_attack: 30,
+    ...
+  },
+  backlinks: {
+    merlin3_rekt: {
+      url: "https://rekt.news/merlin3-rekt/",
+      categories: [
+        "contract_manipulation"
+      ]
+    },
+    safedollar_rekt: {
+      url: "https://rekt.news/safedollar-rekt/",
+      categories: [
+        "polygon",
+        "contract_manipulation"
+      ]
+    },
+    ...
+  }
+}
--- rekt_summarizer/base.py
+++ rekt_summarizer/base.py
@@ -41,38 +41,56 @@ def url_to_markdown(url):
     markdown = convert_to_markdown(article_html)
     return markdown
 
+def single_url_to_markdown(url):
+    if (os.path.exists(article_url_to_filepath(url))):
+        print(f"skipping url: {url} because it already exists")
+        return None
+
+    # sleep so we don't get rate limited
+    time.sleep(5)
+
+    print(f"\nfetching url and converting to markdown: {url}\n")
+    markdown = url_to_markdown(url)
+    return (url, markdown)
 
 def batch_url_to_markdowns(urls):
     raw_markdowns_and_urls = []
     for url in urls:
-        if (os.path.exists(article_url_to_filepath(url))):
-            print(f"skipping url: {url} because it already exists")
-            continue
-
-        print(f"\nfetching url and converting to markdown: {url}\n")
-        markdown = url_to_markdown(url)
-        raw_markdowns_and_urls.append((url, markdown))
-
-        # sleep so we don't get rate limited
-        time.sleep(5)
+        raw_markdown_and_url = single_url_to_markdown(url)
+        if (raw_markdown_and_url is not None):
+            raw_markdowns_and_urls.append(raw_markdown_and_url)
 
     return raw_markdowns_and_urls
 
-def cleanup_markdown_text(markdowns):
-    article_markdown_and_urls = []
-    for (url, markdown) in markdowns:
-        print(f"\ncleaning up the markdown for url: {url}\n")
+def single_cleanup_markdown_text(url, markdown):
+    print(f"\ncleaning up the markdown for url: {url}\n")
 
-        # remove everything after `## SUBSCRIBE NOW`, which we know to be just rekt.news footer text
-        split_markdown = markdown.split('## SUBSCRIBE NOW', 1)
-        markdown = split_markdown[0]
+    # remove everything after `## SUBSCRIBE NOW`, which we know to be just rekt.news footer text
+    split_markdown = markdown.split('## SUBSCRIBE NOW', 1)
+    markdown = split_markdown[0]
 
-        # remove everything before the first `.png)`, which we know to be just rekt.news header text + the URL to the article's PNG header
-        split_markdown = markdown.split(".png)", 1)
-        text = split_markdown[1]
+    # remove everything before the first `.png)`, which we know to be just rekt.news header text + the URL to the article's PNG header
+    # note, this is error-prone, but it's the best we can do for now
+    split_markdown = markdown.split(".png)", 1)
+    if len(split_markdown) != 2:
+        # sometimes rekt.news uses a .jpg instead of a .png for the article header image
+        split_markdown = markdown.split(".jpg)", 1)
+        if len(split_markdown) != 2:
+            # handle https://rekt.news/warp-finance-rekt/
+            if "warp-finance-rekt" not in url:
+                print(f"there is some new parsing error for url: {url}")
+                raise Exception("Error splitting markdown")
+            split_markdown = markdown.split("two...**", 1)
+    text = split_markdown[1]
 
-        article_markdown_and_urls.append((url, text))
-    return article_markdown_and_urls
+    return (url, text)
+
+def batch_cleanup_markdown_text(raw_markdowns_and_urls):
+    article_markdowns_and_urls = []
+    for (url, markdown) in raw_markdowns_and_urls:
+        url_and_text = single_cleanup_markdown_text(url, markdown)
+        article_markdowns_and_urls.append(url_and_text)
+    return article_markdowns_and_urls
 
 def extract_article_urls_from_leaderboard_page(url):
     """
@@ -109,30 +127,41 @@ def article_url_to_filepath(url):
     filepath = os.path.join(MARKDOWN_DIRECTORY, f"{filename_without_extension}.txt")
     return filepath
     
-def write_markdowns_to_files(article_markdown_and_urls):
+
+def single_write_markdown_to_file(url, text_content):
+    filepath = article_url_to_filepath(url)
+
+    # do not bother creating the file if it already exists
+    if not os.path.exists(filepath):
+        with open(filepath, "w") as file:
+            file.write(text_content)
+
+        print(f"Created file: {filepath}")
+
+def batch_write_markdowns_to_files(article_markdown_and_urls):
     # Create the directory if it doesn't exist
     if not os.path.exists(MARKDOWN_DIRECTORY):
         os.makedirs(MARKDOWN_DIRECTORY)
 
     for url, text_content in article_markdown_and_urls:
-
-        filepath = article_url_to_filepath(url)
-
-        # do not bother creating the file if it already exists
-        if not os.path.exists(filepath):
-            with open(filepath, "w") as file:
-                file.write(text_content)
-
-            print(f"Created file: {filepath}")
+        single_write_markdown_to_file(url, text_content)
 
 def main(urls):
     urls = extract_article_urls_from_leaderboard_page("https://rekt.news/leaderboard/")
 
-    raw_markdowns_and_urls = batch_url_to_markdowns(urls)
+    for url in urls:
+        response = single_url_to_markdown(url)
+        if response is None:
+            continue
+        (url, raw_markdown) = response
+        (url, markdown) = single_cleanup_markdown_text(url, raw_markdown)
+        single_write_markdown_to_file(url, markdown)
 
-    article_markdown_and_urls = cleanup_markdown_text(raw_markdowns_and_urls)
+    # raw_markdowns_and_urls = batch_url_to_markdowns(urls)
 
-    write_markdowns_to_files(article_markdown_and_urls)
+    # article_markdown_and_urls = batch_cleanup_markdown_text(raw_markdowns_and_urls)
+
+    # batch_write_markdowns_to_files(article_markdown_and_urls)
     
     # TODO
     # (categories, backlinks) = categorize_markdowns_with_backlinks(markdowns)
